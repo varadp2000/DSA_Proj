5917eef2b141407e8bac560b877edc28a96c30eb
hritikkothari <kotharihrithik1@gmail.com>
2019-10-08 15:20:15 +0530

    COmpleted..

--- First.c
+++ First.c
@@ -5,20 +5,14 @@
 #include<string.h>
 #include<ctype.h>
 
-
 struct users{
-  char fName[20];
-  char lName[20];
-  char username[20];
-  char bdate[10];
-  char password[20];
-  struct users * next;
-};
-
-struct posts{
-  char post[100];
-  char userName[20];
-  struct posts * next;
+    char fName[20];
+    char lName[20];
+    char username[20];
+    char bdate[10];
+    char password[20];
+    int friends[20];
+    struct users * next;
 };
 
 struct users * create_node(){
@@ -28,44 +22,45 @@ struct users * create_node(){
     return newusers;
 }
 
-struct posts * create_post(){
-    struct posts * newpost;
-    newpost=(struct posts *)malloc(sizeof(struct posts));
-    newpost->next=NULL;
-    return newpost;
-}
-
 
 struct users * register_user(struct users * head){
   struct users * tmp;
   tmp=head;
   if(head==NULL){
-    head=create_node();
-    puts("Enter Firstname");
-    scanf("%s",&head->fName);
-    puts("Enter Lastname");
-    scanf("%s",&head->lName);             //register user when head null
-    puts("Enter username");
-    scanf("%s",&head->username);
-    puts("Enter password");
-    scanf("%s",&head->password);
+      head=create_node();
+      puts("Enter Firstname");
+      scanf("%s",&head->fName);
+      puts("Enter Lastname");
+      scanf("%s",&head->lName);             //register user when head null
+      puts("Enter username");
+      scanf("%s",&head->username);
+      puts("Enter password");
+      scanf("%s",&head->password);
+      int i;
+      for(i=0;i<=19;i++){                     //init friends array to 0
+          head->friends[i]=0;
+      }
     return head;
   }
   else{
-  while(tmp->next!=NULL)
-    tmp=tmp->next;
-    tmp->next=create_node();
-    tmp=tmp->next;                          //register user when one or more users exists.
-    puts("Enter Firstname");
-    scanf("%s",&tmp->fName);
-    puts("Enter Lastname");
-    scanf("%s",&tmp->lName);
-    puts("Enter username (Make Sure it does not already exist.)");
-    scanf("%s",&tmp->username);
-    puts("Enter a strong password.");
-    scanf("%s",&tmp->password);
-      return head;
- }
+      while(tmp->next!=NULL)
+          tmp=tmp->next;
+      tmp->next=create_node();
+      tmp=tmp->next;                          //register user when one or more users exists.
+      puts("Enter Firstname");
+      scanf("%s",&tmp->fName);
+      puts("Enter Lastname");
+      scanf("%s",&tmp->lName);
+      puts("Enter username (Make Sure it does not already exist.)");
+      scanf("%s",&tmp->username);
+      puts("Enter a strong password.");
+      scanf("%s",&tmp->password);
+      int i;
+      for(i=0;i<=19;i++){
+          tmp->friends[i]=0;            //init friends array to 0
+      }
+        return head;
+    }
 }
 
 
@@ -132,18 +127,127 @@ struct users * admin_login(struct users * head){          //admin login
               puts("User not Found");
             }
           }
+        }else{
+          puts("Invalid admin password. better luck next time ");
         }
   return head;
+}
+
+int noOfUsers(struct users * head){
+    int no = 1;
+    struct users * tmp;
+    tmp=head;
+    if(head==NULL)
+        return 0;
+    else{
+        while(tmp->next!=NULL){
+            no++;
+            tmp=tmp->next;
+        }
+      return no;
+    }
+
+}
+
+struct users * addFriend(struct users * head,struct users * tmp){
+  struct users * tmp1;
+  char uname[20];
+  tmp1=head;
+    puts("These are the current users.");
+    puts("FirstName\t\t\tLastname\t\t\tUsername\n___________________________________________________________________________");
+      while(tmp1!=NULL){
+        printf("%s\t\t\t\t%s\t\t\t\t%s",tmp1->fName,tmp1->lName,tmp1->username);
+        puts("\n");
+        tmp1=tmp1->next;
+      }
+    puts("Enter username of the person who you want to add.");
+    scanf("%s",uname);
+
+  tmp1=head;
+  int counter=0;
+  int i;
+    while(tmp1!=NULL){
+
+        if(strcmp(uname,tmp1->username)==0){
+            puts("Found user to be added");
+              i=0;
+                while(tmp->friends[i]!=0){
+                  i++;
+                }
+                tmp->friends[i]=counter;
+              break;
+        }
+        counter++;
+        tmp1=tmp1->next;
+
+    }
+
+    int size=0;
+    struct users * tmp2;
+    tmp2=head;
+      while(tmp2!=tmp){
+        tmp2=tmp2->next;
+        size++;
+      }
+
+
+  //add the current user to friend list of the one he just added.
+    i=0;
+    tmp2=head;
+      while(i<counter){
+          tmp2=tmp2->next;
+          i++;
+      }
+    int j=0;
+      while(tmp2->friends[j]!=0){
+          j++;
+      }
+      tmp2->friends[j]=size;
+
+  return head;
+}
+
+void findFriend(struct users * head, int posi){      //traverse to posi and print user
+
+    struct users * tmp;
+    tmp=head;
+    int i=0;
+    while(i<posi)
+      {
+        tmp=tmp->next;
+        i++;
+      }
+
+    printf("%s \n",tmp->username);
+}
+
+void checkFriends(struct users * head,struct users * tmp){
+    struct users * tmp1;
+    tmp1=head;
+    int i;
+    int counter = 0;
+    for(i=0;i<20;i++){
+      if(tmp->friends[i]!=0){
+        counter = 1;                                  // user has friends
+        break;
+      }
+    }
+    if(counter == 0)
+      puts("You dont have any friends.");
+
+    if(counter == 1){                                         // display all friends
+        puts("Your friends are");
+        for(i=0; i<20; i++){
+              if(tmp->friends[i]!=0){
+                  findFriend(head,tmp->friends[i]);        //print who the user is .
+              }
+        }
+    }
 }
 
 
 struct users * login_user(struct users * head){
     struct users * tmp;
-    struct posts * p; // head of posts
-    p=NULL;
-    struct posts * ptemp,*ptemp1;  // temporary of posts struct
-    ptemp=p;
-    ptemp1=p;
     char user[20];
     char pass[20];
     tmp=head;
@@ -152,35 +256,22 @@ struct users * login_user(struct users * head){
     puts("Enter password");
     scanf("%s",&pass);
     int choice;
+    int userflag = 0;
        while(tmp!=NULL){
             if(strcmp(user,tmp->username)==0  )
             {    if(strcmp(pass,tmp->password)==0)   //found user in LL
               {
                 puts("found user");
+                userflag = 1;
                   do{
-                      puts("\n Enter 1 to check all posts \n Enter 2 to add a post \n Enter 3 to logout. ");
+                      puts("\n Enter 1 to check your current friends \n Enter 2 to add a new friend \n Enter 3 to logout. ");
                       scanf("%d",&choice);
                       switch (choice) {
-                        case 1: if(p!=NULL){
-                                    while(ptemp!=NULL){
-                                        printf("%s : \n ",ptemp->userName);
-                                        printf("%s",ptemp->post);
-                                        ptemp=ptemp->next;
-                                    }
-                                 }
+                        case 1:     //check current friends
+                                checkFriends(head,tmp);
                                 break;
-                        case 2: if(p==NULL)
-                                    {
-                                      p=create_post();
-                                      puts("Enter Your Post..");
-                                      scanf("%s",p->post);
-                                    }
-                                    else{
-                                      while(ptemp1->next!=NULL)
-                                          ptemp1=ptemp1->next;
-                                      puts("Enter Your Post..");
-                                      scanf("%s",ptemp1->post);
-                                    }
+                        case 2:     //add a new friend
+                                head=addFriend(head,tmp);
                                 break;
                         case 3:
                                 puts("Logging outt.....");
@@ -188,32 +279,23 @@ struct users * login_user(struct users * head){
                         default: puts("Invalid choice??");
                       }
                   }while(choice!=3);
-              }}else{
-                puts("Invalid Username Or Password");
-              }
+              }}
             tmp=tmp->next;
      }
+    if(userflag == 0){
+        puts("Invalid Username Or Password");
+    }
  return head;
 }
 
-void print(struct users * head)                          //print entire linkedlist:temporaray for debugging
-{
-struct users *tmp;
-tmp=head;
-while(tmp->next!=NULL)
-{
-printf("%s \t",tmp->username);
-tmp=tmp->next;
-}
-printf("%s \t",tmp->username);
-}
 
 void main(){
     struct users * head;
     head=NULL;
     int choice;
-    do{ puts("\n HV's Social Media App");
-        puts("\n Enter 1 to Register \n Enter 2 to Login \n Enter 3 for Admin Login \n Enter 4 to Exit \n enter 5 t print");
+    do{
+        puts("\n Welcome to HV's Social Media App");
+        puts("\n Enter 1 to Register \n Enter 2 to Login \n Enter 3 for Admin Login \n Enter 4 to Exit\n");
         scanf("%d",&choice);
 
         switch (choice){
@@ -225,8 +307,6 @@ void main(){
                   break;
           case 4:
                   puts("Goodbye.");
-                  break;
-          case 5:print(head);               //temporary case for debugging.
                   break;
           default:
                   puts("Invalid Choice.");
